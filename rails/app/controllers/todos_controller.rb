973ce424cd33d608f69fb0f58383cd6a3721c05a
Justin Searls <searls@gmail.com>
2014-10-21 00:26:40 -0500

    remove the rails app to avoid confusion

--- rails/app/controllers/todos_controller.rb
+++
@@ -1,9 +0,0 @@
-class TodosController < ApplicationController
-  def index
-    @todos = []
-  end
-
-  def create
-    @todo = {description: params['description'], done: true.to_s == params['done']}
-  end
-end
--- rails/spec/javascripts/example-spec.coffee
+++
@@ -1,5 +0,0 @@
-describe 'the 21st century', ->
-  Given -> @date = new Date()
-  When -> @result = @date.getFullYear()
-  Then -> @result > 2000
-  And -> @result <= 2100
