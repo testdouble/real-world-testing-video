8da538feaf3eefcac177d03821af16a14a660945
David Mosher <davidmosher@gmail.com>
2015-04-30 16:43:36 -0400

    remove rails/legacy code examples

--- rails/app/views/todos/create.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @todo, :description, :done
--- rails/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Example::Application.routes.draw do
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  resources :todos
-
-  root 'todos#index'
-end
